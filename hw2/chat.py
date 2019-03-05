2438487fb176f6b2a5b51ceae380b43ddcc62c70
Sam Eakin <sseakin@gmail.com>
2019-03-05 16:31:26 -0600

    before class

--- hw2/chat.py
+++ hw2/chat.py
@@ -13,7 +13,7 @@ py chat.py user1 5001 127.0.0.1:5002 127.0.0.1:5003
 
 import sys
 import socket
-import selectors
+import select
 
 HOST = '127.0.0.1' # The server's hostname or IP address
 
@@ -24,7 +24,8 @@ class Client:
 		self.IP = None
 		self.connections = []
 		self.sel = None
-		self.lsock = None
+		self.lsock = None # socket client will listen on.
+		self.wsock = None # socket client will send on.
 		self.message = ''
 
 	# 1[chat.py] 2[name] 3[port] 4[connection:port] 5[connection:port]
@@ -33,6 +34,7 @@ class Client:
 
 		if arg_count < 3:
 			print("Not enough arguments!")
+			sys.exit(-1)
 		elif arg_count == 3:
 			print("Please enter connections!")
 		else:
@@ -42,58 +44,55 @@ class Client:
 			arguments = sys.argv[3:]
 			for arg in arguments:
 				arg = arg.split(':')
-				self.connections.append(arg[1])
+				self.connections.append(int(arg[1]))
 
 	def createListenSocket(self):
 		print('Setting up listening socket:')
-		self.sel = selectors.DefaultSelector()
 		self.lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.lsock.bind((self.IP, self.port))
 		self.lsock.listen()
 		print('Listening on ',(self.IP, self.port))
-		self.lsock.setblocking(False)
-		self.sel.register(self.lsock, selectors.EVENT_READ, data=None)
 
 	def createMessage(self):
-		print('Enter your message: ')
+		print('Welcome to the Chat: ')
 		message = input()
 		message = message.encode()
 		self.message = message
 
 	def sendMessage(self):
 		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-			s.connect((self.IP, int(self.connections[0])))
+			s.connect((self.IP, self.connections[0]))
 			s.sendall(self.message)
+			#data = s.recv(1024)
+			#print ('Recieved', repr(data))
 
 	def getMessage(self):
-		conn, addr = lsock.accept() 
+		self.wsock, addr = self.lsock.accept() 
 		# The with statement is used with conn to automatically close the socket at the end of the block.
-		with conn:
-			print('Connected by', addr)
-			while True:
-				data = conn.recv(1024)
-				if not data:
-					break
-				print(data)
-				conn.sendall(data)
+		print('Connected by', addr)
+		while True:
+			data = self.wsock.recv(1024)
+			if not data:
+				break
+			print(data)
+			self.wsock.sendall(data)
 	
 	def __str__(self):
 		print('Client Initialized:')
 		return '%s %s %s %s' % (self.name, self.port, self.IP, self.connections)
 
-class Packet:
-	pass
-
 if __name__ == "__main__":
-	# parse command line arguments
 	user = Client(sys.argv[1])
 	user.getInput()
 	print(user)
 
 	user.createListenSocket()
 
+	# ---- This is where I'm stuck ---- #
+	# How do I create an event loop that will print any messages recieved
+	# over the listening socket? It also has to be waiting for any typed input to send.
+
 	user.createMessage()
-	print(user.message)
 
 	user.sendMessage()
 
