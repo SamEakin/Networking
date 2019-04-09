e9f36a523e68efba9071a2b83092bff1447df437
Sam Eakin <sseakin@gmail.com>
2019-04-09 13:38:47 -0500

    Parses HTTP request.

--- hw4/server.py
+++ hw4/server.py
@@ -4,7 +4,11 @@
 # 4/8/19
 
 '''
+--start server--
 python server.py 127.0.0.1:8000
+
+--web input--
+http://127.0.0.1:8000/car.html
 '''
 
 import sys
@@ -27,29 +31,49 @@ def getInputArgs():
 		return IP, port
 
 def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print('accepted connection from', addr)
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
+	conn, addr = sock.accept()  # Should be ready to read
+	print('accepted connection from', addr)
+	conn.setblocking(False)
+	data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+	events = selectors.EVENT_READ | selectors.EVENT_WRITE
+	sel.register(conn, events, data=data)
 
 def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print('closing connection to', data.addr)
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print('echoing', repr(data.outb), 'to', data.addr)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
+	sock = key.fileobj
+	data = key.data
+	if mask & selectors.EVENT_READ:
+		recv_data = sock.recv(1024)  # Should be ready to read
+		if recv_data:
+			data.outb += recv_data
+		else:
+			print('closing connection to', data.addr)
+			sel.unregister(sock)
+			sock.close()
+	if mask & selectors.EVENT_WRITE:
+		if data.outb:
+			process_http_header(sock, data.outb)
+			#print('echoing', repr(data.outb), 'to', data.addr)
+			sent = sock.send(data.outb)  # Should be ready to write
+			data.outb = data.outb[sent:]
+
+def process_http_header(socket, data):
+	header = str(data.decode())
+	#print(header)
+	header_items = header.split('\r\n')
+	request_item = header_items[0]
+	host_item = header_items[1]
+	request_item = request_item.split(' ')
+	host_item = host_item.split(': ')
+
+	host = host_item[1]
+	print("****REQUEST****")
+	print(request_item)
+	print("****URI****")
+	print(host)
+
+def print_items(stuff):
+	for item in stuff:
+		print(item)
 
 ##########################
 
@@ -64,13 +88,12 @@ lsock.setblocking(False)
 sel.register(lsock, selectors.EVENT_READ, data=None)
 
 while True:
-    events = sel.select(timeout=None)
-    for key, mask in events:
-        if key.data is None:
-            accept_wrapper(key.fileobj)
-        else:
-            service_connection(key, mask)
+	events = sel.select(timeout=None)
+	for key, mask in events:
+		if key.data is None:
+			accept_wrapper(key.fileobj)
+		else:
+			service_connection(key, mask)
 
 
 
- 
