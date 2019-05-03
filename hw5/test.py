1605826dae1b995d0ca1b5cc18fd4b01854e2fe2
Sam Eakin <sseakin@gmail.com>
2019-05-03 16:24:31 -0500

    finished

--- hw5/test.py
+++ hw5/test.py
@@ -1,21 +1,16 @@
+# Networking Homework 5
+#
+#
 import sys
 import json
 from collections import defaultdict
 
 class Graph():
     def __init__(self):
-        """
-        self.edges is a dict of all possible next nodes
-        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
-        self.weights has all the weights between two nodes,
-        with the two nodes as a tuple as the key
-        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
-        """
         self.edges = defaultdict(list)
         self.weights = {}
     
     def add_edge(self, from_node, to_node, weight):
-        # Note: assumes edges are bi-directional
         self.edges[from_node].append(to_node)
         self.edges[to_node].append(from_node)
         self.weights[(from_node, to_node)] = weight
@@ -35,8 +30,6 @@ def getInputArgs(): # py path.py [graph.json] [source] [destination]
 
 
 def dijsktra(graph, initial, end):
-    # shortest paths is a dict of nodes
-    # whose value is a tuple of (previous node, weight)
     shortest_paths = {initial: (None, 0)}
     current_node = initial
     visited = set()
@@ -58,10 +51,8 @@ def dijsktra(graph, initial, end):
         next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
         if not next_destinations:
             return "Route Not Possible"
-        # next node is the destination with the lowest weight
         current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
     
-    # Work back through destinations in shortest path
     path = []
     while current_node is not None:
         path.append(current_node)
@@ -70,41 +61,22 @@ def dijsktra(graph, initial, end):
     # Reverse path
     path = path[::-1]
     return path
+
+
 ####################
 
 file_name, source, destination = getInputArgs()
 
 network = Graph()
 edges = []
-'''edges = [
-    ('X', 'A', 7),
-    ('X', 'B', 2),
-    ('X', 'C', 3),
-    ('X', 'E', 4),
-    ('A', 'B', 3),
-    ('A', 'D', 4),
-    ('B', 'D', 4),
-    ('B', 'H', 5),
-    ('C', 'L', 2),
-    ('D', 'F', 1),
-    ('F', 'H', 3),
-    ('G', 'H', 2),
-    ('G', 'Y', 2),
-    ('I', 'J', 6),
-    ('I', 'K', 4),
-    ('I', 'L', 4),
-    ('J', 'L', 1),
-    ('K', 'Y', 5),
-]
-'''
+
 with open(file_name) as json_file:
     data = json.load(json_file)
-    for entry in data['graph']:
+    for entry in data:
         print('src='+ entry['src'] + ' dst='+ entry['dst'] + ' w='+ entry['w'])
         edges.append((entry['src'], entry['dst'], int(entry['w'])))
-        #delete duplicates
 
-for edge in edges:
+for edge in edges: #deletes redudant duplicate edges
     src = edge[0]
     dst = edge[1]
     for otheredge in edges:
