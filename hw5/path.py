d8ad2c7929de0f254f94066d6175d13b557bc6da
Sam Eakin <sseakin@gmail.com>
2019-05-03 11:44:25 -0500

    added command line arg input

--- hw5/path.py
+++ hw5/path.py
@@ -2,6 +2,7 @@
 # Networking Homework 5
 # Sam Eakin
 
+import sys
 import json
 
 class Graph:
@@ -12,7 +13,6 @@ class Graph:
 
 	def nodeExists(self, n):
 		for node in self.nodes:
-			#print('Checking if '+n+' matches '+node.name)
 			if n == node.name:
 				return True
 		else:
@@ -32,11 +32,11 @@ class Graph:
 		for node in self.nodes:
 			print(node)
 
-
 class Node:
 	def __init__(self, name):
 		self.name = name
 		self.edges = [] #list of tuples (dst, w)
+		self.minimumDist = -1
 
 	def addEdge(self, dest, weight):
 		self.edges.append((dest, weight))
@@ -44,12 +44,27 @@ class Node:
 	def __str__(self):
 		return 'Node '+self.name+' edges = '+str(self.edges)
 
+def getInputArgs(): # py path.py [graph.json] [source] [destination]
+		arg_count = len(sys.argv)
+		print(arg_count)
+		if arg_count < 4:
+			print("Not enough arguments!")
+			sys.exit(-1)
+		else:
+			file_name = sys.argv[1]
+			source = sys.argv[2]
+			destination = sys.argv[3]
+			return file_name, source, destination
+
+
 ##########################
 if __name__ == "__main__":
 
+	file_name, source, destination = getInputArgs()
+
 	network = Graph()
 
-	with open('graph.json') as json_file:
+	with open(file_name) as json_file:
 		data = json.load(json_file)
 		for node in data['graph']:
 			print('src='+ node['src'] + ' dst='+ node['dst'] + ' w='+ node['w'])
@@ -59,5 +74,6 @@ if __name__ == "__main__":
 			else:
 				print('Node Does Not Exist. Creating new Node.')
 				network.createNode(node['src'], node['dst'], node['w'])
-		network.print()
+	
+	network.print()
 
